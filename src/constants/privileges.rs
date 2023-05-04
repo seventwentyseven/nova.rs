38812035505107c341f650501e381362a9e74038
def750 <def75023@gmail.com>
2023-05-04 21:56:06 +0000

    beautify

--- src/constants/privileges.rs
+++ src/constants/privileges.rs
@@ -3,40 +3,40 @@ use bitflags::bitflags;
 bitflags! {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
     pub struct Privileges: u32 {
-        const Unrestricted = 1<<0;
-        const Verified = 1<<1;
-        const Frozen = 1<<2;
-        const Szef = 1<<3;
+        const Unrestricted = 1 << 0;
+        const Verified = 1 << 1;
+        const Frozen = 1 << 2;
+        const Szef = 1 << 3;
 
-        const WhitelistedStdVn = 1<<4;
-        const WhitelistedTaikoVn = 1<<5;
-        const WhitelistedCatchVn = 1<<6;
-        const WhitelistedManiaVn = 1<<7;
-        const WhitelistedStdRx = 1<<8;
-        const WhitelistedTaikoRx = 1<<9;
-        const WhitelistedCatchRx = 1<<10;
-        const WhitelistedStdAp = 1<<11;
+        const WhitelistedStdVn = 1 << 4;
+        const WhitelistedTaikoVn = 1 << 5;
+        const WhitelistedCatchVn = 1 << 6;
+        const WhitelistedManiaVn = 1 << 7;
+        const WhitelistedStdRx = 1 << 8;
+        const WhitelistedTaikoRx = 1 << 9;
+        const WhitelistedCatchRx = 1 << 10;
+        const WhitelistedStdAp = 1 << 11;
 
-        const Supporter = 1<<12;
-        const Alumni = 1<<13;
-        const TourneyManager = 1<<14;
+        const Supporter = 1 << 12;
+        const Alumni = 1 << 13;
+        const TourneyManager = 1 << 14;
 
-        const NominatorStd = 1<<15;
-        const NominatorTaiko = 1<<16;
-        const NominatorCatch = 1<<17;
-        const NominatorMania = 1<<18;
+        const NominatorStd = 1 << 15;
+        const NominatorTaiko = 1 << 16;
+        const NominatorCatch = 1 << 17;
+        const NominatorMania = 1 << 18;
 
-        const QatStd = 1<<19;
-        const QatTaiko = 1<<20;
-        const QatCatch = 1<<21;
-        const QatMania = 1<<22;
+        const QatStd = 1 << 19;
+        const QatTaiko = 1 << 20;
+        const QatCatch = 1 << 21;
+        const QatMania = 1 << 22;
 
-        const Moderator = 1<<23;
-        const CommunityManager = 1<<24;
-        const Administrator = 1<<25;
-        const HeadAdmin = 1<<26;
-        const Developer = 1<<27;
-        const Owner = 1<<28;
+        const Moderator = 1 << 23;
+        const CommunityManager = 1 << 24;
+        const Administrator = 1 << 25;
+        const HeadAdmin = 1 << 26;
+        const Developer = 1 << 27;
+        const Owner = 1 << 28;
 
         const Staff = Self::Moderator.bits()     | Self::CommunityManager.bits() |
                       Self::Administrator.bits() | Self::HeadAdmin.bits() |
@@ -61,12 +61,12 @@ bitflags! {
 bitflags! {
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
     pub struct ClientPrivileges: u8 {
-        const Player = 1 << 0;
-        const Moderator = 1 << 1;
-        const Supporter = 1 << 2;
-        const Owner = 1 << 3;
-        const Developer = 1 << 4;
-        const Tournament = 1 << 5;  // NOTE: not used in communications with osu! client
+        const Player = 1  <<  0;
+        const Moderator = 1  <<  1;
+        const Supporter = 1  <<  2;
+        const Owner = 1  <<  3;
+        const Developer = 1  <<  4;
+        const Tournament = 1  <<  5;  // NOTE: not used in communications with osu! client
     }
 }
 
